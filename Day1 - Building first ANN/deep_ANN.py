129c35bc9cabdd75158bd72769eefccf7f9343e9
natiqvahabov <natiqvahabov@gmail.com>
2018-11-11 12:26:18 +0400

    Confusion matrix round added to pred result

--- Day1 - Building first ANN/deep_ANN.py
+++ Day1 - Building first ANN/deep_ANN.py
@@ -39,7 +39,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test) 
+X_test = sc.transform(X_test) 
 
 # keras
 import keras
@@ -70,6 +70,6 @@ y_pred = classifier.predict(X_test)
 # confusion matrix
 from sklearn.metrics import confusion_matrix
 
-cm = confusion_matrix(y_test,y_pred)
+cm = confusion_matrix(y_test,y_pred.round())
 # true outcomes of cm were 84.2 % of all test data 
 # 1684 out of 2000
