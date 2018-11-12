7164fd474ad1cc44e578d39194e1c251d41be34c
natiqvahabov <natiqvahabov@gmail.com>
2018-11-12 15:49:35 +0400

    Dropout regularization added

--- Day3 - ANN Dropout and Parameter tuning/deep_ANN.py
+++ Day3 - ANN Dropout and Parameter tuning/deep_ANN.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  10 12:54:24 2018
+Created on Fri Nov  11 14:24:14 2018
 
 @author: natig
 """
+
+# ------------------------   Day1   -----------------------------
+
 # importing python library for csv table reading - pandas
 import pandas as pd
 
@@ -73,3 +76,48 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred.round())
 # true outcomes of cm were 84.2 % of all test data 
 # 1684 out of 2000
+
+
+# ------------------------   Day2   -----------------------------
+
+# predict new entered row
+new_node_X = [0,0,619,1,40,3,60000,2,1,1,50000]
+new_node_X = sc.transform(np.reshape(new_node_X,(11,1)).T)
+new_row_pred = classifier.predict(new_node_X)
+new_row_pred = (new_row_pred>0.5)
+print(new_row_pred) # False
+
+# Evaluating the ANN, 10 fold cross validation
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units=6, init='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(units=6, init='uniform', activation='relu'))
+    classifier.add(Dense(units=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, epochs=100)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10,n_jobs=1)
+mean = accuracies.mean() # 0.8352499946579337
+variance = accuracies.std() # 0.012634278179076582
+
+
+# ------------------------   Day3   -----------------------------
+
+from keras.layers import Dropout
+
+#adding dropout to first 2 hidden layers
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units=6, init='uniform', activation='relu', input_dim=11))
+    classifier.add(Dropout(p=0.1))
+    classifier.add(Dense(units=6, init='uniform', activation='relu'))
+    classifier.add(Dropout(p=0.1))
+    classifier.add(Dense(units=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
