c265d0a0752d93037c024fef2283a92606b653d2
natiqvahabov <natiqvahabov@gmail.com>
2018-11-18 03:04:57 +0400

    added RNN layers,fit and compile

--- Day8 - Building first RNN/build_rnn.py
+++ Day8 - Building first RNN/build_rnn.py
@@ -37,3 +37,39 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 
 # reshape dataset, keras needs (batch_size, timestemp, input_dim)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+# Part2 - Building the RNN
+
+# importing keras libraries
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+# initialize regressor
+regressor = Sequential()
+
+# adding first LSTM layer, and Dropout regularization
+# 3 inputs important: number_of_lstm_cells, return_sequence for several lstm layers, input_shape 3D
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# adding second LSTM layer, and Dropout regularization
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# adding third LSTM layer, and Dropout regularization
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# adding forth LSTM layer, and Dropout regularization
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+# adding output layer
+regressor.add(Dense(units=1))
+
+# compiling the RNN, RMSprop is also good choice for RNN
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+# fitting RNN to the TrainingSet
+regressor.fit(X_train, y_train, epochs=100, batch_size=32)
