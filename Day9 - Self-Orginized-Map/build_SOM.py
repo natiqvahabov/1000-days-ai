b4569fe4aa83a809d85f9e0b302b997c064fe034
natiqvahabov <natiqvahabov@gmail.com>
2018-11-19 15:32:11 +0400

    finding frauds for bank

---
+++ Day9 - Self-Orginized-Map/build_SOM.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 12:18:16 2018
+
+@author: natig
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing csv dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# feature scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+# Training SOM with third library package minisom
+from minisom import MiniSom
+som = MiniSom(x=10, y=10, input_len=15, sigma=1.0, learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration=100)
+
+# visualizing SOM
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize=10,
+         markeredgewidth=2)
+show()
+
+# finding the frauds
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(3, 2)], mappings[(8, 5)], mappings[(8, 8)]), axis=0)
+frauds = sc.inverse_transform(frauds)
